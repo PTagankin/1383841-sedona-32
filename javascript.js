1f0d2703cf87a368ec0686b2656bf48ceea50609
PTagankin <90925165+PTagankin@users.noreply.github.com>
2021-11-01 17:09:28 +0300

    доработал javascript

--- javascript.js
+++ javascript.js
@@ -1,19 +1,24 @@
-function myFunction() {
-    var element = document.getElementById("myDIV");
-    element.className = element.className.replace(/\bmodal-closed\b/g, "");
-  }
+const modalOpen = document.querySelector('.search');
+const modal = document.querySelector('.modal-container');
+const modalClose = document.querySelector('.modal-close-button');
+const popups = document.querySelector('.popup');
+const popup = document.querySelector('.popover');
 
-  function modalClose() {
-    var element = document.getElementById("myDIV");
-    element.classList.add("modal-closed");
-  }
 
-  function popupOpen() {
-    var element = document.getElementById("pop-up");
-    element.className = element.className.replace(/\bpopover-disable\b/g, "");
-  }
+function handler1() {
+  popup.classList.remove('popover-disable');
+}
 
-  function popupClose() {
-    var element = document.getElementById("pop-up");
-    element.classList.add("popover-close");
-  }
+function handler2() {
+  popup.classList.add('popover-disable');
+}
+
+modalOpen.addEventListener('click', function() {
+  modal.classList.remove('modal-closed');
+}) 
+
+modalClose.addEventListener('click', function(){
+  modal.classList.add('modal-closed');
+})
+
+popups.addEventListener('click', handler1);
